2af70f055151071aff8f9244f6c37aeb5e18855f
Elena Zhuvak <elenkazh@gmail.com>
2024-09-29 16:03:18 +0100

    adds HTTP-queries

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,43 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
 const BASIC_URL = 'https://pixabay.com/api/';
 const KEY_API = '46224221-307a76b395d66c959be951e41';
 
+export function fetchImages(query) {
+    const searchParams = new URLSearchParams({
+        key: KEY_API,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+    });
 
-
-
-// const options = {
-//     key: KEY_API,
-//     q: input,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: 'true',
-// }
+    return fetch(`${BASIC_URL}?${searchParams}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status)
+        }
+        return response.json()
+    })
+    .then(data => {
+        const images = data.hits;
+        if (images.length === 0) {
+            iziToast.error({
+                title: 'Error',
+                message: "Sorry, there are no images matching your search query. Please try again!",
+                position: "topRight",
+            });
+            return [];
+        }
+        return images;
+    })
+    .catch(error => {
+        iziToast.error({
+            title: 'Error',
+            message: 'Illegal operation',
+            position: "topRight",
+        });
+        return []
+    });
+}
--- src/main.js
+++ src/main.js
@@ -3,13 +3,26 @@ import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+import {fetchImages} from './js/pixabay-api'
+
 const form = document.querySelector('.js-search-form');
 const gallery = document.querySelector('.gallery');
 
 form.addEventListener('submit', onSubmit);
+
 function onSubmit (event) {
     event.preventDefault();
-    const query = form.elements.input.value;
-    console.log(query);
+    const query = form.elements.searchInput.value.trim();
+
+    if (query === '') {
+        iziToast.warning({
+            message: 'Please fill in the field',
+            position: "topRight",
+        });
+        return;
+    }
+    fetchImages(query)
+
+    form.reset()
 }
-onSubmit("dog");
+
